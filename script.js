3dab1f39963e679742eae15184daf0d552f9c1b0
Samir Chokshi <samirchokshi11@gmail.com>
2024-04-26 06:58:34 +0000

    Finished

---
+++ script.js
@@ -0,0 +1,10 @@
+let array=[0,1]
+
+let count=1000
+for (let i = 2; i < count; i++) {
+    array.push(array[i-2]+array[i-1])
+}
+
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i+1]/array[i])
+}
