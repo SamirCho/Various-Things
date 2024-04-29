e289eedd6085f27675f730a8ea1f065fc9f4f762
Samir Chokshi <samirchokshi11@gmail.com>
2024-04-29 04:01:14 +0000

    cc

---
+++ cc.js
@@ -0,0 +1,18 @@
+function cc(x,n){
+    if(x==1){
+        console.log("Done for "+n)
+    }else if(x%2==0){
+        x/=2
+        console.log(x)
+        cc(x,n)       
+    }else if(x%2==1){
+        x=3*x+1
+        console.log(x)
+        cc(x,n)
+    }
+}
+
+for (let i = 1; i < 2000000; i++) {
+    console.log(i)
+    cc(i,i)
+}
